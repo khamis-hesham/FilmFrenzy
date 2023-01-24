a00f54f0c880fce5a5dd9c7c9afb4884faf3c128
KhamisAlBashiti <kalbashiti-e@jahez.net>
2023-01-25 01:19:08 +0200

    structure

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <p>Welcooooooooooooome</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
